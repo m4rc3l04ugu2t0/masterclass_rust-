a9e63c548678d785e6528ae64522444ead1e68af
Marcelo <mrcelo014@gmail.com>
2024-05-10 21:42:53 -0300

    associated types

--- src/traits/associated_types.rs
+++ src/traits/associated_types.rs
@@ -1,6 +1,31 @@
 trait Compute {
-    type Target;
-    fn compute(&self, rhs: Self::Target) -> Self::Target;
+	type Target;
+	fn compute(&self, rhs: Self::Target) -> Self::Target;
 }
 
-pub fn execute() {}
+struct Add(i32);
+struct Sub(f32);
+
+impl Compute for Add {
+	type Target = i32;
+	fn compute(&self, rhs: Self::Target) -> Self::Target {
+		self.0 + rhs
+	}
+}
+
+impl Compute for Sub {
+	type Target = f32;
+	fn compute(&self, rhs: Self::Target) -> Self::Target {
+		self.0 + rhs
+	}
+}
+
+pub fn execute() {
+	let add = Add(1);
+	let two = add.compute(1);
+	println!("add.compute => {}\n", two);
+
+	let sub = Sub(1.0);
+	let zero = sub.compute(1.0);
+	println!("sub.compute => {}", zero);
+}
